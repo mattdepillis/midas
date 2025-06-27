d71c4c4d1c580ecd8cd5def4604fe031a45879c1
Matt DePillis <mattdepillis@gmail.com>
2025-06-27 12:18:14 -0400

    new model updates for additional coingecko data

--- src/models/portfolio.py
+++ src/models/portfolio.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Optional
 
 from pydantic import BaseModel  # type: ignore
@@ -13,11 +14,36 @@ class CryptoAsset(BaseModel):
     is_staked: bool
     apy: Optional[float] = None
     estimated_annual_yield_usd: Optional[float] = 0
-    price_change_24h: Optional[float] = None  # % change
-    price_change_percentage_24h: Optional[float] = None
-    price_change_7d: Optional[float] = None  # % change
-    price_change_percentage_7d_in_currency: Optional[float] = None
-    price_change_percentage_7d_in_currency: Optional[float] = None
-    market_cap: Optional[int] = 0
+
+    # --- New Enriched Fields ---
     image: Optional[str] = None
-    last_updated: Optional[str] = None
+    market_cap: Optional[float] = None
+    market_cap_rank: Optional[int] = None
+    fully_diluted_valuation: Optional[float] = None
+    total_volume: Optional[float] = None
+
+    high_24h: Optional[float] = None
+    low_24h: Optional[float] = None
+    price_change_24h: Optional[float] = None
+    price_change_percentage_24h: Optional[float] = None
+    market_cap_change_24h: Optional[float] = None
+    market_cap_change_percentage_24h: Optional[float] = None
+
+    circulating_supply: Optional[float] = None
+    total_supply: Optional[float] = None
+    max_supply: Optional[float] = None
+
+    ath: Optional[float] = None
+    ath_change_percentage: Optional[float] = None
+    ath_date: Optional[datetime] = None
+    atl: Optional[float] = None
+    atl_change_percentage: Optional[float] = None
+    atl_date: Optional[datetime] = None
+
+    price_change_percentage_7d: Optional[float] = None
+    price_change_percentage_14d: Optional[float] = None
+    price_change_percentage_30d: Optional[float] = None
+    price_change_percentage_200d: Optional[float] = None
+    price_change_percentage_1y: Optional[float] = None
+
+    last_updated: Optional[datetime] = None

7a4c8891c0cbb074f9f60be78c1f074e740cb4cc
Matt DePillis <mattdepillis@gmail.com>
2025-07-10 20:06:41 -0400

    added insights api route

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,8 +1,15 @@
-from typing import List
+from typing import List, Union
 
 from fastapi import APIRouter, Query  # type: ignore
 
-from models.portfolio_assets import CryptoAsset
+# insights classes/runners
+from insights.base import InsightResult
+from insights.runner import generate_insights
+
+# portfolio asset classes
+from models.portfolio_assets import CryptoAsset, FiatAsset
+
+# portfolio services
 from services.portfolio import get_portfolio
 
 router = APIRouter()
@@ -16,3 +23,16 @@ async def fetch_holdings(
     if staked_only:
         portfolio = [asset for asset in portfolio if asset.is_staked]
     return portfolio
+
+
+@router.get("/insights", response_model=List[InsightResult])
+async def get_insights():
+    # Step 1: Get enriched asset holdings
+    holdings: List[Union[CryptoAsset, FiatAsset]] = await get_portfolio()
+
+    # Step 2: Run insights (filter to crypto only)
+    crypto_holdings = [a for a in holdings if isinstance(a, CryptoAsset)]
+    insights = generate_insights(crypto_holdings)
+
+    # Step 3: Return results
+    return insights
